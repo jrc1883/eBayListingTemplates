e933f930e24291238d462a9ff54a6c9912969096
jrc1883 <joseph.cannon@outlook.com>
2022-12-20 14:16:29 -0600

    Create eBay.js

---
+++ eBay.js
@@ -0,0 +1,12 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const eBay = 'https://www.ebay.com/sch/i.html?_dkr=1&iconV2Request=true&_blrs=recall_filtering&_ssn=thehouseofdealsco&store_cat=0&store_name=thehouseofdealsco&_oac=1&_nkw=';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = eBay + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
